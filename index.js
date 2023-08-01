9e7d4fad9e16c58d9615ff26a383d8d0029e272c
Ilari Metsälä <ilari.metsala@aalto.fi>
2023-08-01 22:40:17 +0300

    a

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 morgan.token('content', function getContent (req) {
   if(req.method === 'POST')
   return JSON.stringify(req.body)
