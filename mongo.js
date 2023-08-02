5764c6cc93a3b61929ea0d5860096955c9095917
Ilari Metsälä <ilari.metsala@aalto.fi>
2023-08-02 16:17:19 +0300

    3.12

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+    `mongodb+srv://ilari:${password}@phonebook.mivldyu.mongodb.net/persons?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+})
+const printPhonebook = () => {
+    Person.find({}).then(result => {
+        result.forEach(note => {
+            console.log(note)
+        })
+        mongoose.connection.close()
+    })
+}
+
+person.save().then(result => {
+    if (!process.argv[3]) {
+        printPhonebook()
+    } else {
+        console.log(`added ${person.name} number ${person.number} to phonebook`)
+        mongoose.connection.close()
+    }
+})
